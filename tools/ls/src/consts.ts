529bac7fede3ed7e29ec1cdef0987ebff49084c1
giveerr <givessomeerr.e@gmail.com>
2025-07-13 01:45:18 +0300

    fix(std/logger): export of transports changed

--- tools/ls/src/consts.ts
+++ tools/ls/src/consts.ts
@@ -2,6 +2,6 @@ import { homedir } from 'node:os'
 import { join } from 'node:path'
 
 import { createLogger } from '@ozaco/std/logger'
-import { createFileTransport } from '@ozaco/std/logger-file'
+import { createFileTransport } from '@ozaco/std/logger/file'
 
 export const logger = createLogger('tools/ls').use('file', createFileTransport(join(homedir(), '.ozaco/logs')))
