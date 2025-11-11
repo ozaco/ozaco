586146db21716e2cea3f07717441bd795b274a94
giveerr <givessomeerr.e@gmail.com>
2025-11-11 21:35:44 +0300

    refactor(std/shared): unsafe utils

--- packages/std/src/shared/index.ts
+++ packages/std/src/shared/index.ts
@@ -3,4 +3,4 @@ export * from './types/common'
 export * from './utils/is'
 export * from './utils/timing'
 
-export * from './utils/unsafe/id-generator'
+export * as unsafe from './utils/unsafe'
--- packages/std/src/shared/utils/unsafe/id-generator.ts
+++ packages/std/src/shared/utils/unsafe/id-generator.ts
@@ -1,4 +1,4 @@
-export const unsafeIdGenerator = function* (prefix: string) {
+export const idGenerator = function* (prefix: string) {
   let nextId = 0
   while (true) {
     yield `${prefix}:${++nextId}:${Date.now()}`
