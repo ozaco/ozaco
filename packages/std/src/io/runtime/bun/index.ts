370e9d9b2a5119d3704f608b89479706da538eeb
antaresrvish <yildirimyusuf758@gmail.com>
2026-02-02 16:41:55 +0300

    feat(std/io): add exists for bun runtime

--- packages/std/src/io/runtime/bun/index.ts
+++ packages/std/src/io/runtime/bun/index.ts
@@ -2,11 +2,12 @@ import { extendableIO } from 'std:io'
 import { createPlugin } from 'std:plugin'
 
 import { stats } from '../node/stats'
+import { exists } from './exists'
 import { open } from './open'
 import { read } from './read'
 import { write } from './write'
 
-export const createBunIO = createPlugin(extendableIO.clone().define(stats, open, read, write), {
+export const createBunIO = createPlugin(extendableIO.clone().define(stats, open, read, write, exists), {
   name: 'bun',
 })
 
